2e3a58f2653e06955206a86721d7964251fd96a2
Golem <elfwriterbluemoon@gmail.com>
2026-07-30 12:45:34 -0500

    Update index-sonidos.js

--- index-sonidos.js
+++ index-sonidos.js
@@ -2,7 +2,7 @@ console.log("clicksound.js conectado");
 
 
 const boton = document.getElementById("click1");
-const sonido = new Audio("click1.mp3");
+const sonido = new Audio("burbuja.mp3");
 
 boton.addEventListener("click", () => {
     sonido.currentTime = 0;
